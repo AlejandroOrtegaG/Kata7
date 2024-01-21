6acd87fc065403cc9c8d40caa31dae56501dfc35
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-01-21 19:56:12 +0000

    Estructura del proyecto

--- src/main/java/kata7/Main.java
+++ src/main/java/kata7/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package kata7;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new MainFrame().setVisible(true);
     }
 }
---
+++ src/main/java/kata7/MainFrame.java
@@ -0,0 +1,14 @@
+package kata7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() throws HeadlessException {
+        this.setTitle("Kata 7");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(400,400);
+        this.setLocationRelativeTo(null);
+    }
+}
